3cc6ea7f405eed7c4be87950b88aab1cbd115bb7
'schrocat' <15hyxue@stu.edu.cn>
2018-08-06 14:11:30 +0800

    否

--- distributed-monitor/src/main/java/json/util/StatusCode.java
+++ distributed-monitor/src/main/java/json/util/StatusCode.java
@@ -2,6 +2,6 @@ package json.util;
 
 public class StatusCode {
 
-	public static final String CODE_SUCCESS = "200";
-	public static final String CODE_ERROR ="400";
+	public static final String CODE_SUCCESS = "1";
+	public static final String CODE_ERROR ="0";
 }
--- distributed-monitor/src/main/java/service/action/MonitorToolAction.java
+++ distributed-monitor/src/main/java/service/action/MonitorToolAction.java
@@ -7,15 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import service.implement.GetMessageTypeServiceImpl;
+
 @Controller
 public class MonitorToolAction {
-	@RequestMapping(value="/data.do", method=RequestMethod.POST)
+	@RequestMapping(value="/data.do", method=RequestMethod.GET)
 	public void getdata(HttpServletRequest request,HttpServletResponse response){
 		int type=0;
 		type = Integer.parseInt(request.getParameter("type"));
 		switch(type){
 		case 1:
 			new ServiceAvaliabilityAction().getAviliableService(request, response);
+			break;
+		case 2:
+			new GetSendWayAction().getSendWay(response, request);
+			break;
+		case 3:
+			new GetMessageTypeAction().getMessageType(response, request);;
+			break;
+		case 4:
+		case 5:
+			new BusinessRateAction().getBusinessRateService(response, request, type);
 			break;
 		default:
 			new ErrorAction().getError(request, response);
